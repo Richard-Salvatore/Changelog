73b8ba9ca187fe85cdde3b3647e31cf9df274633
Richard-Salvatore <166079004+Richard-Salvatore@users.noreply.github.com>
2025-03-05 02:52:06 +0100

    Update Salvatore.lua

--- Salvatore.lua
+++ Salvatore.lua
@@ -1,15 +1,1 @@
-Fluent:Notify({
-    Title = "Changelog - v0.77",
-    Content = "Brookhaven RP:\n" ..
-             "• Some bug fixes.\n" ..
-             "• Added lock player.\n" ..
-             "• Added throw player.\n" ..
-             "• Added arrest player.\n" ..
-             "• Added fe lag server.\n" ..
-             "• Added multi basketball.\n" ..
-             "• Added loop throw basketball.\n" ..
-             "• Rewritten rainbow functions.\n" ..
-             "• Rewritten multi tools functions.\n" ..
-             "• Rewritten kill, bring functions.",
-    Duration = 30
-})
+
